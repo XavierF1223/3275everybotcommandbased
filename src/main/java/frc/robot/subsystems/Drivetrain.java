4aa7f7dcb2364537eec15e407ca3641411073866
XavierF1223 <123787517+XavierF1223@users.noreply.github.com>
2023-02-25 02:46:41 -0600

    setup work

--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -4,21 +4,24 @@
 
 package frc.robot.subsystems;
 
-import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonFX;
+
 import frc.robot.Constants.DriveConstants;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.math.util.Units;
+
 import com.kauailabs.navx.frc.AHRS;
 import edu.wpi.first.wpilibj.SPI;
-import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
-import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.math.util.Units;
 
 public class Drivetrain extends SubsystemBase {
   /** Creates a new Drivetrain. */
-  private WPI_TalonFX FL;
-  private WPI_TalonFX FR;
-  private WPI_TalonFX RL;
-  private WPI_TalonFX RR;
+  private TalonFX FL = new TalonFX(DriveConstants.FL);
+  private TalonFX FR = new TalonFX(DriveConstants.FR);
+  private TalonFX RL = new TalonFX(DriveConstants.RL);
+  private TalonFX RR = new TalonFX(DriveConstants.RR);
   private final AHRS m_navx = new AHRS(SPI.Port.kMXP, (byte) 200); // NavX connected over MXP
 
   final int kCountsPerRev = 4096;  //Encoder counts per revolution of the motor shaft.
@@ -55,8 +58,8 @@ public class Drivetrain extends SubsystemBase {
   }
 
   public void setMotors(double left, double right){
-    FL.set(left);
-    FR.set(-right);
+    FL.set(ControlMode.PercentOutput, left);
+    FR.set(ControlMode.PercentOutput, -right);
   }
 
   @Override
